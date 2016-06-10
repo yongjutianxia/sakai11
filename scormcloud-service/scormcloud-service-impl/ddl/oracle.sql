cc3b6ba2aa75f36ff995d1f09d84bd6f2d865ef3
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 06:50:52 +0000

    CLASSES-1849 Fix DDL for Oracle--missing uuid column
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@324171 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- scormcloud-service/scormcloud-service-impl/ddl/oracle.sql
+++ scormcloud-service/scormcloud-service-impl/ddl/oracle.sql
@@ -1,6 +1,6 @@
 create table scs_scorm_job (uuid varchar2(36) primary key, siteid varchar2(36), externalid varchar2(255), resourceid varchar2(255), title varchar2(255), graded int, ctime number, mtime number, retry_count int default 0, status varchar2(32), deleted int default 0);
 create table scs_scorm_course (uuid varchar2(36) primary key, siteid varchar2(36), externalid varchar2(255), resourceid varchar2(255), title varchar2(255), graded int, ctime number, mtime number, deleted int);
-create table scs_scorm_registration (courseid varchar2(36), userid varchar2(36), ctime number, mtime number);
+create table scs_scorm_registration (uuid varchar(36) primary key, courseid varchar2(36), userid varchar2(36), ctime number, mtime number);
 create table scs_scorm_job_info (jobname varchar2(36) primary key, last_run_time number);
 create table scs_scorm_scores (registrationid varchar2(36) primary key, score double precision);
 
