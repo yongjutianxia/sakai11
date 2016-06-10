49857f450c90d707752db9fd43c42a7fe1659dd8
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 07:01:52 +0000

    CLASSES-1630 Print stack trace for null launch data for tracking SCORM issue for individuals
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@324251 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- scorm/scorm-tool/src/java/org/sakaiproject/scorm/ui/player/ScormSecurityAdvisor.java
+++ scorm/scorm-tool/src/java/org/sakaiproject/scorm/ui/player/ScormSecurityAdvisor.java
@@ -13,22 +13,31 @@ public class ScormSecurityAdvisor implements SecurityAdvisor {
 	public SecurityAdvice isAllowed(String userId, String function, String reference) {
 		log.debug("isAllowed: userId="+userId+", function="+function+", reference="+reference);
 		if (ContentHostingService.AUTH_RESOURCE_READ.equals(function)) {
+			if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- AUTH_RESOURCE_READ"); }
 			if (SecurityService.unlock(userId, "scorm.launch", currentSiteReference()) || SecurityService.unlock(userId, "scorm.upload", currentSiteReference())) {
+				if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- AUTH_RESOURCE_READ -- allowed"); }
 				return SecurityAdvice.ALLOWED;
 			}
 		} else if (ContentHostingService.AUTH_RESOURCE_HIDDEN.equals(function)) {
+			if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- AUTH_RESOURCE_HIDDEN"); }
 			if (SecurityService.unlock(userId, "scorm.launch", currentSiteReference()) || SecurityService.unlock(userId, "scorm.upload", currentSiteReference())) {
+				if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- AUTH_RESOURCE_HIDDEN -- allowed"); }
 				return SecurityAdvice.ALLOWED;
 			}
 		} else if (ContentHostingService.AUTH_RESOURCE_ADD.equals(function) || ContentHostingService.AUTH_RESOURCE_WRITE_ANY.equals(function) || ContentHostingService.AUTH_RESOURCE_WRITE_OWN.equals(function)) {
+			if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- AUTH_RESOURCE_ADD"); }
 			if (SecurityService.unlock(userId, "scorm.upload", currentSiteReference())) {
+				if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- AUTH_RESOURCE_ADD -- allowed"); }
 				return SecurityAdvice.ALLOWED;
 			}
 		} else if (ContentHostingService.AUTH_RESOURCE_REMOVE_ANY.equals(function) || ContentHostingService.AUTH_RESOURCE_REMOVE_OWN.equals(function)) {
+			if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- AUTH_RESOURCE_REMOVE_ANY"); }
 			if (SecurityService.unlock(userId, "scorm.delete", currentSiteReference())) {
+				if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- AUTH_RESOURCE_REMOVE_ANY -- allowed"); }
 				return SecurityAdvice.ALLOWED;
 			}
 		}
+		if (log.isDebugEnabled()) { log.debug("NYU-SCORM-DEBUG -- returning PASS"); }
 		return SecurityAdvice.PASS;
 	}
 	private String currentSiteReference() {
