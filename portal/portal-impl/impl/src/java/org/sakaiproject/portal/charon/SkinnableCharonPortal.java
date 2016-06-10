9f03ae94f3d931183044858d9d842c28b8df247f
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 06:27:36 +0000

    CLASSES-1412 Build external help by default, and use it in the portal

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
@@ -107,7 +107,8 @@ import org.sakaiproject.portal.util.ToolURLManagerImpl;
 import org.sakaiproject.portal.util.ToolUtils;
 import org.sakaiproject.portal.util.URLUtils;
 import org.sakaiproject.portal.util.PortalUtils;
-import org.sakaiproject.portal.util.ExternalHelpSystem;
+import edu.nyu.classes.externalhelp.api.ExternalHelpSystem;
+import edu.nyu.classes.externalhelp.api.ExternalHelp;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SitePage;
 import org.sakaiproject.site.api.ToolConfiguration;
@@ -740,8 +741,8 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 			}
 
 
-			ExternalHelpSystem.ExternalHelp help = externalHelpSystem.getHelp(placement.getToolId(), userRole);
-			ExternalHelpSystem.ExternalHelp news = externalHelpSystem.getNews(placement.getToolId(), userRole);
+			ExternalHelp help = externalHelpSystem.getHelp(placement.getToolId(), userRole);
+			ExternalHelp news = externalHelpSystem.getNews(placement.getToolId(), userRole);
 
 			toolMap.put("usingExternalHelp", Boolean.valueOf(true));
 			toolMap.put("externalHelp", help);
@@ -2084,7 +2085,7 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 		// warning messages will appear, but the end state will be the same.
 		portalService.addPortal(this);
 
-		externalHelpSystem = new ExternalHelpSystem();
+		externalHelpSystem = (ExternalHelpSystem) ComponentManager.get("edu.nyu.classes.externalhelp.api.ExternalHelpSystem");
 
 		worksiteHandler = new WorksiteHandler();
 		siteHandler = new SiteHandler();
