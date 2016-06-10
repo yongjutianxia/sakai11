94d7cbf18eec4527b611784cd516b76e50069e7d
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 06:37:21 +0000

    CLASSES-1607 Use a mousewheel jquery library to deal with inconsistencies between browsers.
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@324085 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- gradebook/app/ui/src/webapp/js/spreadsheetUI.js
+++ gradebook/app/ui/src/webapp/js/spreadsheetUI.js
@@ -1,3 +1,13 @@
+/*! Copyright (c) 2013 Brandon Aaron (http://brandon.aaron.sh)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Version: 3.1.12
+ *
+ * Requires: jQuery 1.2.2+
+ */
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?module.exports=a:a(jQuery)}(function(a){function b(b){var g=b||window.event,h=i.call(arguments,1),j=0,l=0,m=0,n=0,o=0,p=0;if(b=a.event.fix(g),b.type="mousewheel","detail"in g&&(m=-1*g.detail),"wheelDelta"in g&&(m=g.wheelDelta),"wheelDeltaY"in g&&(m=g.wheelDeltaY),"wheelDeltaX"in g&&(l=-1*g.wheelDeltaX),"axis"in g&&g.axis===g.HORIZONTAL_AXIS&&(l=-1*m,m=0),j=0===m?l:m,"deltaY"in g&&(m=-1*g.deltaY,j=m),"deltaX"in g&&(l=g.deltaX,0===m&&(j=-1*l)),0!==m||0!==l){if(1===g.deltaMode){var q=a.data(this,"mousewheel-line-height");j*=q,m*=q,l*=q}else if(2===g.deltaMode){var r=a.data(this,"mousewheel-page-height");j*=r,m*=r,l*=r}if(n=Math.max(Math.abs(m),Math.abs(l)),(!f||f>n)&&(f=n,d(g,n)&&(f/=40)),d(g,n)&&(j/=40,l/=40,m/=40),j=Math[j>=1?"floor":"ceil"](j/f),l=Math[l>=1?"floor":"ceil"](l/f),m=Math[m>=1?"floor":"ceil"](m/f),k.settings.normalizeOffset&&this.getBoundingClientRect){var s=this.getBoundingClientRect();o=b.clientX-s.left,p=b.clientY-s.top}return b.deltaX=l,b.deltaY=m,b.deltaFactor=f,b.offsetX=o,b.offsetY=p,b.deltaMode=0,h.unshift(b,j,l,m),e&&clearTimeout(e),e=setTimeout(c,200),(a.event.dispatch||a.event.handle).apply(this,h)}}function c(){f=null}function d(a,b){return k.settings.adjustOldDeltas&&"mousewheel"===a.type&&b%120===0}var e,f,g=["wheel","mousewheel","DOMMouseScroll","MozMousePixelScroll"],h="onwheel"in document||document.documentMode>=9?["wheel"]:["mousewheel","DomMouseScroll","MozMousePixelScroll"],i=Array.prototype.slice;if(a.event.fixHooks)for(var j=g.length;j;)a.event.fixHooks[g[--j]]=a.event.mouseHooks;var k=a.event.special.mousewheel={version:"3.1.12",setup:function(){if(this.addEventListener)for(var c=h.length;c;)this.addEventListener(h[--c],b,!1);else this.onmousewheel=b;a.data(this,"mousewheel-line-height",k.getLineHeight(this)),a.data(this,"mousewheel-page-height",k.getPageHeight(this))},teardown:function(){if(this.removeEventListener)for(var c=h.length;c;)this.removeEventListener(h[--c],b,!1);else this.onmousewheel=null;a.removeData(this,"mousewheel-line-height"),a.removeData(this,"mousewheel-page-height")},getLineHeight:function(b){var c=a(b),d=c["offsetParent"in a.fn?"offsetParent":"parent"]();return d.length||(d=a("body")),parseInt(d.css("fontSize"),10)||parseInt(c.css("fontSize"),10)||16},getPageHeight:function(b){return a(b).height()},settings:{adjustOldDeltas:!0,normalizeOffset:!0}};a.fn.extend({mousewheel:function(a){return a?this.bind("mousewheel",a):this.trigger("mousewheel")},unmousewheel:function(a){return this.unbind("mousewheel",a)}})});
+
+
 var el1, el2, els = null;
 adjustScrolls = function(){
    el1.style.left = -els.scrollLeft + 'px';
@@ -102,6 +112,34 @@ $(document).ready(gethandles);
 // Bind a mouse wheel listener for the left hand pane so scrolling works consistently.
 $(function () {
 
+    var reset_row_heights = function () {
+      $("#q3 tr").each(function(i){
+          thisHeight = $(this).height();
+          thatHeight = $("#q4 tr:eq(" + i + ")").height();
+
+          var new_height = Math.max(thisHeight, thatHeight) + 5;
+
+          $("#q4 tr:eq(" + i + ")").height(new_height);
+          $(this).height(new_height);
+        })
+
+      // If we're not showing a scrollbar (such as on OSX), make set
+      // the heights to take up the full space available
+      var q3_div = $('#q3 > div');
+      var q4_div = $('#q4 > div');
+
+      if (q4_div.height() === q4_div[0].clientHeight) {
+        // what a world.
+        q3_div.height(q4_div.height());
+      }
+    };
+     
+    reset_row_heights();
+    // setInterval(reset_row_heights, 1000);
+
+
+    var divToScroll = $('#q4 > div');
+
     // This function checks if the specified event is supported by the browser.
     // Source: http://perfectionkills.com/detecting-event-support-without-browser-sniffing/
     function isEventSupported(eventName) {
@@ -116,66 +154,20 @@ $(function () {
         return isSupported;
     }
 
-
-    var reset_row_heights = function () {
-      $("#q3 tr").each(function(i){
-          thisHeight = $(this).height();
-          thatHeight = $("#q4 tr:eq(" + i + ")").height();
-
-          var new_height = Math.max(thisHeight, thatHeight) + 5;
-
-          $("#q4 tr:eq(" + i + ")").height(new_height);
-          $(this).height(new_height);
-        })
-
-      // If we're not showing a scrollbar (such as on OSX), set the heights to
-      // take up the full space available
-      var q3_div = $('#q3 > div');
-      var q4_div = $('#q4 > div');
-
-      if (q4_div.height() === q4_div[0].clientHeight) {
-        // what a world.
-        q3_div.height(q4_div.height());
-      }
-    };
-
-    reset_row_heights();
-
-
-    var wheelEvent = isEventSupported('mousewheel') ? 'mousewheel' : 'wheel';
-    var divToScroll = $('#q4 > div');
-
-    $('#q3,#q4').on(wheelEvent, function(e) {
-
-        var oEvent = e.originalEvent;
-        var deltaY = oEvent.deltaY;
-        var deltaX = oEvent.deltaX || 0;
-
-        if (deltaY === undefined) {
-          deltaY = oEvent.wheelDelta;
-          deltaX = 0;
-        }
-
+    $('#q3,#q4').on('mousewheel', function(event) {
         var leftPosition = divToScroll.scrollLeft();
         var topPosition = divToScroll.scrollTop();
 
-
-        if (oEvent.deltaMode === 1) {
-          // Firefox defaults to line-based scrolling, so we need to accelerate it.
-          deltaX *= 20;
-          deltaY *= 20;
-        }
-
         // Pass the scroll to the other pane
-        if (deltaX) {
-          divToScroll.scrollLeft(leftPosition + deltaX);
+        if (event.deltaX) {
+          divToScroll.scrollLeft(leftPosition + (event.deltaX * event.deltaFactor));
+        }
+        
+        if (event.deltaY) {
+          divToScroll.scrollTop(topPosition - (event.deltaY * event.deltaFactor));
         }
 
-        if (deltaY) {
-          divToScroll.scrollTop(topPosition + deltaY);
-        }
-
-        e.preventDefault();
+        event.preventDefault();
         return false;
     });
 });
