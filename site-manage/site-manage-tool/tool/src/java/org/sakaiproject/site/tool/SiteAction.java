187904a4314f962d6a33b266bf7990cbe5b9c9d6
mt1970@nyu.edu <mt1970@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-06-10 06:35:58 +0000

    CLASSES-1579 Don't notify when templates are used.

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -6947,7 +6947,8 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 				String newSiteIconUrl = transferSiteResource(templateSite.getId(), site.getId(), site.getIconUrl());
 				site.setIconUrl(newSiteIconUrl);
 				
-				userNotificationProvider.notifyTemplateUse(templateSite, UserDirectoryService.getCurrentUser(), site);	
+                                // CLASSES-1579: Nope.
+				// userNotificationProvider.notifyTemplateUse(templateSite, UserDirectoryService.getCurrentUser(), site);	
 			}
 				
 			ResourcePropertiesEdit rp = site.getPropertiesEdit();
