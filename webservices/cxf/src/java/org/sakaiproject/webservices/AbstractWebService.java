a745e15e0a2b40a902dcbd625035d539496061d7
Mark Triggs <mark@dishevelled.net>
2016-06-22 14:14:13 +1000

    UPGRADE Knock out the bits of the Longsight webservices that we don't use (and which don't build)

--- webservices/cxf/src/java/org/sakaiproject/webservices/AbstractWebService.java
+++ webservices/cxf/src/java/org/sakaiproject/webservices/AbstractWebService.java
@@ -97,7 +97,6 @@ public class AbstractWebService {
     protected QuestionPoolServiceAPI questionPoolServiceImpl;
     protected LessonBuilderAccessAPI lessonBuilderAccessAPI;
     protected ArchiveService archiveService;
-    protected PreferencesService preferencesService;
     protected MemoryService memoryService;
     protected SqlService sqlService;
     protected ImportService importService;
@@ -280,15 +279,11 @@ public class AbstractWebService {
     @WebMethod(exclude = true)
     public void setQuestionPoolServiceImpl(QuestionPoolServiceAPI questionPoolServiceImpl) {
         this.questionPoolServiceImpl = questionPoolServiceImpl;
+    }
 
     @WebMethod(exclude = true)
     public void setArchiveService(ArchiveService archiveService) {
         this.archiveService = archiveService;
-    }
-    
-    @WebMethod(exclude = true)
-    public void setPreferencesService(PreferencesService preferencesService) {
-        this.preferencesService = preferencesService;
     }
     
     @WebMethod(exclude = true)
