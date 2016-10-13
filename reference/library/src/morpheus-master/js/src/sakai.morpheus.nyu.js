9c6d4089d0a63460b3b98ecbd8408cc9b5a3b373
Payten Giles <payten.giles@gmail.com>
2016-10-13 12:42:41 +1100

    CLASSES-2316 ensure footer is slammed on bottom of window by calculating the tool panel height to take into account both the tool menu height and viewport height. Also add a border-left to the footer and ensure the body background color is agreeable.

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.nyu.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.nyu.js
@@ -1,9 +1,24 @@
-/* CLASSES-2264 CLASSES-1319 NYU When neo templates are used, ensure the content pane matches the tool menu height */
+/* CLASSES-2264 CLASSES-1319 NYU When neo templates are used,
+   ensure the content pane matches the tool menu height
+*/
 $(function() {
   var $toolMenu = $("#toolMenu");
   var $portlet = $("#content");
 
   if ($portlet.length == 1) {
-    $portlet.css("minHeight", $toolMenu.height() + "px");
+    var menuHeight = $toolMenu.height();
+
+    // CLASSES-2316 We want to ensure the footer is planted at the bottom
+    // of the windows so ensure the min-height of the portlet panel is enough
+    // to do this
+    if ($(document.body).height() < $(window).height()) {
+      var footerHeight = $("#footer").outerHeight();
+      var headerHeight = $portlet.offset().top;
+      var calculatedHeight = $(window).height() - headerHeight -  footerHeight;
+
+      menuHeight = Math.max(menuHeight, calculatedHeight);
+    }
+
+    $portlet.css("minHeight", menuHeight + "px");
   }
 });
