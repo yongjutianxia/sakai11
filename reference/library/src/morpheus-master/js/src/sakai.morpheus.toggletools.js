e43249e2ee890aa0daa1a14432f027ef81170320
Mark Triggs <mark@dishevelled.net>
2016-10-13 12:53:35 +1100

    CLASSES-2314 Ensure collapse tool menu persists between refreshes

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.toggletools.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.toggletools.js
@@ -1,29 +1,41 @@
 /**
-* For toggling the Minimize and Maximize tools menu in Morpheus: Adds classes to the <body> and changes the label text for accessibility
+* For toggling the Minimize and Maximize tools menu in Morpheus.
 */
 
 function toggleMinimizeNav(){
-
   $PBJQ('body').toggleClass('Mrphs-toolMenu-collapsed');
 
+  var isCollapsed = $PBJQ('body').hasClass('Mrphs-toolMenu-collapsed');
+
   var el = $PBJQ(this);
-  var label = $PBJQ('.accessibility-btn-label' , el);
 
-  if (label.text() == el.data("title-expand")) {
-    label.text(el.data("text-original"));
-    el.attr('title', (el.data("text-original")));
-    el.attr('aria-pressed', true);
-    document.cookie = "sakai_nav_minimized=false; path=/";
-
-  } else {
-
-    el.data("text-original", label.text());
-    label.text(el.data("title-expand"));
+  if (isCollapsed) {
     el.attr('title', (el.data("title-expand")));
     el.attr('aria-pressed', false);
     document.cookie = "sakai_nav_minimized=true; path=/";
-
+  } else {
+    el.attr('title', (el.data("text-original")));
+    el.attr('aria-pressed', true);
+    document.cookie = "sakai_nav_minimized=false; path=/";
   }
 }
 
 $PBJQ(".js-toggle-nav").on("click", toggleMinimizeNav);
+
+/* Backport SAK-31456 */
+$PBJQ(document).ready(function(){
+  if(getCookieVal('sakai_nav_minimized') === 'true') {
+    $PBJQ(".js-toggle-nav").click();
+  }
+});
+
+function getCookieVal(cookieName) {
+  var cks = document.cookie.split(';');
+  for (var i = 0; i < cks.length; ++i) {
+    var curCookie = (cks[i].substring(0,cks[i].indexOf('='))).trim();
+    if(curCookie === cookieName) {
+      return ((cks[i].split('='))[1]).trim();;
+    }
+  }
+  return undefined;
+}
