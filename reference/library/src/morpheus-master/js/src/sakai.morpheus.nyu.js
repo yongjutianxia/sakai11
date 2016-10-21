1eb518d9ef084a92fb639f4eddb5a6879b704295
Payten Giles <payten.giles@gmail.com>
2016-10-21 12:01:15 +1100

    NOJIRA Implement a single "mobile" breakpoint based on the new NYU skin designs

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.nyu.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.nyu.js
@@ -2,8 +2,8 @@
    ensure the content pane matches the tool menu height
 */
 $(function() {
-  var $toolMenu = $("#toolMenu");
-  var $portlet = $("#content");
+  var $toolMenu = $PBJQ("#toolMenu");
+  var $portlet = $PBJQ("#content");
 
   if ($portlet.length == 1) {
     var menuHeight = $toolMenu.height();
@@ -11,14 +11,31 @@ $(function() {
     // CLASSES-2316 We want to ensure the footer is planted at the bottom
     // of the windows so ensure the min-height of the portlet panel is enough
     // to do this
-    if ($(document.body).height() < $(window).height()) {
-      var footerHeight = $("#footer").outerHeight();
+    if ($PBJQ(document.body).height() < $PBJQ(window).height()) {
+      var footerHeight = $PBJQ("#footer").outerHeight();
       var headerHeight = $portlet.offset().top;
-      var calculatedHeight = $(window).height() - headerHeight -  footerHeight;
+      var calculatedHeight = $PBJQ(window).height() - headerHeight -  footerHeight;
 
       menuHeight = Math.max(menuHeight, calculatedHeight);
     }
 
     $portlet.css("minHeight", menuHeight + "px");
   }
+});
+
+// Allow custom tool button to toggle tool nav too
+$(function() {
+  $PBJQ(".Mrphs-siteHierarchy").prepend("<a href='javascript:void' aria-hidden='true' id='nyuToolToggle'>Tools</a>");
+  $PBJQ("#nyuToolToggle").on("click", toggleToolsNav);
+});
+
+// Setup banner logo to return to top of page upon click
+$(function() {
+  $PBJQ(".Mrphs-headerLogo").on("click", function() {
+    // only do this if in mobile view
+    if ($(".nyu-desktop-only:first").is(":not(:visible)")) {
+      document.body.scrollTop = 0;
+      document.body.scrollLeft = 0;
+    }
+  });
 });
