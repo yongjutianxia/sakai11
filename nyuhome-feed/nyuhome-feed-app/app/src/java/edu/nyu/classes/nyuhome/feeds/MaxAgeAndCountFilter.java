42c5c9d661c1f033bded76cefb0910917d9b48c7
pg1025@nyu.edu <pg1025@nyu.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-11-04 01:56:51 +0000

    CLASSES-2322 change MaxAgeAndCountFilter to limit based on accepted items rather than items seen
    git-svn-id: https://source.sakaiproject.org/svn/msub/nyu.edu/10.7/trunk@325365 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- nyuhome-feed/nyuhome-feed-app/app/src/java/edu/nyu/classes/nyuhome/feeds/MaxAgeAndCountFilter.java
+++ nyuhome-feed/nyuhome-feed-app/app/src/java/edu/nyu/classes/nyuhome/feeds/MaxAgeAndCountFilter.java
@@ -5,24 +5,27 @@ import java.util.Date;
 
 class MaxAgeAndCountFilter {
     private Date expireDate;
-    private int resultsSeen;
+    private int acceptedResultsCount;
     private int maxResults;
 
     public MaxAgeAndCountFilter(int maxAgeDays, int maxResults) {
-        this.resultsSeen = 0;
+        this.acceptedResultsCount = 0;
         this.maxResults = maxResults;
 
         expireDate = new Date(new Date().getTime() - ((long)maxAgeDays * 24 * 60 * 60 * 1000));
     }
 
     public boolean accept(Date entryDate) {
-        if (resultsSeen >= maxResults) {
+        if (acceptedResultsCount >= maxResults) {
             return false;
-        } else {
-            resultsSeen++;
         }
 
-        return entryDate.after(expireDate);
+        if (entryDate.after(expireDate)) {
+            acceptedResultsCount++;
+            return true;
+        }
+
+        return false;
     }
 }
 
