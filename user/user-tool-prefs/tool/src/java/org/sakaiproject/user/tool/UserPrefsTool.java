b5b0d982a58c69d2fda4b4064f300c903c5e72e2
Mark Triggs <mark@dishevelled.net>
2016-12-22 15:31:39 +1100

    CLASSES-2590 Order the list of hidden site terms by descending start date

--- user/user-tool-prefs/tool/src/java/org/sakaiproject/user/tool/UserPrefsTool.java
+++ user/user-tool-prefs/tool/src/java/org/sakaiproject/user/tool/UserPrefsTool.java
@@ -67,6 +67,9 @@ import org.sakaiproject.util.ResourceLoader;
 import org.sakaiproject.util.FormattedText;
 import org.sakaiproject.util.Web;
 
+import org.sakaiproject.coursemanagement.api.CourseManagementService;
+import org.sakaiproject.coursemanagement.api.AcademicSession;
+
 /**
  * UserPrefsTool is the Sakai end-user tool to view and edit one's preferences.
  */
@@ -2549,6 +2552,8 @@ public class UserPrefsTool
 	{
 		private List<Term> terms;
 
+		private CourseManagementService courseManagementService = (CourseManagementService) ComponentManager.get(CourseManagementService.class);
+
 		public class Term implements Comparable<Term> {
 			private String label;
 			private List<Site> sites;
@@ -2615,9 +2620,41 @@ public class UserPrefsTool
 				terms.add(new Term(name, termsToSites.get(name)));
 			}
 
-			Collections.sort(terms);
+			sortTerms();
 		}
 
+
+		private void sortTerms() {
+			if (courseManagementService == null) {
+				Collections.sort(terms);
+			} else {
+				List<AcademicSession> sessions = courseManagementService.getAcademicSessions();
+				final List<String> sessionTitles = new ArrayList<>();
+
+				for (AcademicSession session : sessions) {
+					sessionTitles.add(session.getTitle());
+				}
+
+				Collections.sort(terms, new Comparator<Term>() {
+					public boolean equals(Term us, Term them) {
+						return us.equals(them);
+					}
+
+					public int compare(Term us, Term them) {
+						int ourPos = sessionTitles.indexOf(us.getLabel());
+						int theirPos = sessionTitles.indexOf(them.getLabel());
+
+						if (ourPos == -1) {
+							return 1;
+						} else if (theirPos == -1) {
+							return -1;
+						} else {
+							return Integer.compare(ourPos, theirPos);
+						}
+					}
+				});
+			}
+		}
 
 		public List<Term> getTerms() {
 			return terms;
