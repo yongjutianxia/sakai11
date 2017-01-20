ec30604c2546990cd898552b13a1c0cb33540b69
Payten Giles <payten.giles@gmail.com>
2017-01-20 14:09:17 +1100

    Revert "CLASSES-2533 ensure selected delegated access  menu items don't look pants"
    This commit contained changes beyond that of CLASSES-2533 - my mistake!
    
    This reverts commit a4be22b90d40c6895e10f021cfbb3833801f9f88.

--- delegatedaccess/tool/src/java/org/sakaiproject/delegatedaccess/tool/pages/BasePage.java
+++ delegatedaccess/tool/src/java/org/sakaiproject/delegatedaccess/tool/pages/BasePage.java
@@ -269,7 +269,7 @@ public class BasePage extends WebPage implements IHeaderContributor {
 	 */
 	protected void disableLink(Link<Void> l) {
 		l.add(new AttributeAppender("class", new Model<String>("current"), " "));
-		l.replace(l.get(0));
+		l.setRenderBodyOnly(true);
 		l.setEnabled(false);
 	}
 
--- reference/library/src/webapp/js/settings-renamer.js
+++ reference/library/src/webapp/js/settings-renamer.js
@@ -11,30 +11,13 @@ function renameSettingsToJoinable(title) {
 
 
 var findSettingsMenuLink = function () {
-    var toolMenu = $('#toolMenu');
-    var elt = $('.icon-sakai-siteinfo', toolMenu).closest('.Mrphs-toolsNav__menuitem--link');
+    var elt = $('.icon-sakai-siteinfo').closest('.Mrphs-toolsNav__menuitem--link');
 
     if (elt.length > 0) {
         return elt;
     } else {
         return undefined;
     }
-};
-
-var prepareTheToolPage = function(tool) {
-    // change the header site-hierarchy refresh button (mobile)
-    var navReset = $('.Mrphs-siteHierarchy .Mrphs-hierarchy-item.Mrphs-hierarchy--toolName');
-    navReset.find('span:last-child').text(SETTINGS_TOOL_LABEL);
-    navReset.find('.icon-sakai-siteinfo').removeClass('icon-sakai-siteinfo').addClass('icon-sakai-joinable-groups');
-
-    // change the tool-scoped refresh button (desktop)
-    var nyuRefresh = $('.nyu-desktop-only .Mrphs-hierarchy-item.Mrphs-hierarchy--toolName');
-    nyuRefresh.find('span:last-child').text(SETTINGS_TOOL_LABEL);
-    nyuRefresh.find('.icon-sakai-siteinfo').removeClass('icon-sakai-siteinfo').addClass('icon-sakai-joinable-groups');
-
-    // hide things in the sites table
-    $('.summary-mathjax-allowed', tool).remove();
-    $('.summary-instruction-mode', tool).remove();
 };
 
 var switchToJoinableGroups = function (link) {
@@ -67,9 +50,9 @@ $(document).ready(function() {
     if (showJoinableGroups) {
         switchToJoinableGroups(settingsTool);
 
-        var tool = $('.Mrphs-sakai-siteinfo');
+        var tool = $('.tool-sakai-siteinfo');
         if (tool.length > 0) {
-            prepareTheToolPage(tool);
+            tool.find('.portletTitle .title h2').text(SETTINGS_TOOL_LABEL);
         }
     } else {
         $(settingsTool).closest('li').hide();
