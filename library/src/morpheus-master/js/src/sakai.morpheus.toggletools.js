1c5483d47a6ab1fc568c851961de67d4189f8347
Mark Triggs <mark@dishevelled.net>
2017-02-28 10:45:23 +1100

    Floating tool menu collapse

--- library/src/morpheus-master/js/src/sakai.morpheus.toggletools.js
+++ library/src/morpheus-master/js/src/sakai.morpheus.toggletools.js
@@ -11,12 +11,8 @@ function toggleMinimizeNav(){
   var el = $PBJQ(this);
   var label = $PBJQ('.accessibility-btn-label' , el);
 
-  el.toggleClass('min max');
-  
-  if (label.text() == el.data("title-expand") || collapsed) {
-	document.cookie = "sakai_nav_minimized=false; path=/";
-	collapsed = false;
-	label.text(el.data("text-original"));
+  if (label.text() == el.data("title-expand")) {
+    label.text(el.data("text-original"));
     el.attr('title', (el.data("text-original")));
     el.attr('aria-pressed', true);
   } else {
@@ -30,44 +26,3 @@ function toggleMinimizeNav(){
 }
 
 $PBJQ(".js-toggle-nav").on("click", toggleMinimizeNav);
-
-var collapsed = false;
-
-var $window = $PBJQ(window),
-	$tools	= $("#toolMenu"),
-	padding	= $(".Mrphs-siteHierarchy").height() + $(".Mrphs-topHeader").height();
-
-$PBJQ(document).ready(function(){
-	if(getCookieVal('sakai_nav_minimized') === 'true') {
-		$PBJQ(".js-toggle-nav").click();
-		collapsed = true;
-	}
-});
-
-function animateToolBar() {
-	if($("#toolMenuWrap").attr("scrollingToolbarEnabled") != undefined){
-		var topPad = $(".pasystem-banner-alerts").height();
-		var follow = ($window.height()- (padding + topPad)) > $tools.height();
-		if($("#toolMenuWrap").css('position') !== 'fixed'
-			&& follow && $window.scrollTop() > 0) {
-			$("#toolMenu,#subSites").stop().animate({
-				top: $window.scrollTop() + topPad
-			});
-		} else {
-			$("#toolMenu,#subSites").stop().animate({
-				top: 0
-			});
-		}
-	}
-}
-
-function getCookieVal(cookieName) {
-	var cks = document.cookie.split(';');
-	for (var i = 0; i < cks.length; ++i) {
-		var curCookie = (cks[i].substring(0,cks[i].indexOf('='))).trim();
-		if(curCookie === cookieName) {
-			return ((cks[i].split('='))[1]).trim();;
-		}
-	}
-	return undefined;
-}
---
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.toolmenu-collapse.js
@@ -0,0 +1,19 @@
+jQuery(function () {
+    var collapse = $('.Mrphs-collapseTools');
+    if (collapse.length > 0) {
+        var floatIt = function () {
+            var width = $('#toolMenuWrap').outerWidth();
+
+            collapse.css('position', 'fixed')
+                    .css('bottom', 0)
+                    .css('left', 0)
+                    .css('width', width);
+
+            collapse.addClass('floatingToolMenu');
+        };
+
+        floatIt();
+
+        collapse.on('click', floatIt);
+    }
+});
